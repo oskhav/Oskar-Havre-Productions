49285e1b11755f419947b4533cf14e79af65ad19
oskhav <oskarhavre@gmail.com>
2025-04-10 09:43:31 +0200

    Fikset et lite problem hvor server.js klagde på at den ikke fant filene. Det var fordi 'views' mappen het 'view' istedet for 'views'

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 
 // Sett opp EJS som view engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+//app.set("views", path.join(__dirname, "views"));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
